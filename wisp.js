8c3852914f402368aedbcb591ba7ed9d0a958260
imruckingfetarded <49215868+imruckingfetarded@users.noreply.github.com>
2019-05-18 01:29:09 -0500

    Update wisp.js

--- wisp.js
+++ wisp.js
@@ -4,6 +4,18 @@ require("dotenv/config");
 
 //the client
 const bot = new Discord.Client({disableEveryone: true});
+//on functions
+bot.on("ready", async () => {
+    console.log("[WISP] I am now online!")
+    bot.user.setActivity(`to your bullshit`, {type: "LISTENING"});
+});
+
+
+
+
+
+
+
 
 //imported settings
 let prefix;
