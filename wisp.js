fd4c4ae59cb8876026b4669308dd769f1e17b26a
imruckingfetarded <49215868+imruckingfetarded@users.noreply.github.com>
2019-05-18 01:46:09 -0500

    Service account added

--- wisp.js
+++ wisp.js
@@ -7,18 +7,28 @@ const bot = new Discord.Client({disableEveryone: true});
 //on functions
 bot.on("ready", async () => {
     console.log("[WISP] I am now online!")
-    bot.user.setActivity(`to your bullshit`, {type: "LISTENING"});
+    bot.user.setActivity(`your bullshit`, {type: "LISTENING"});
 });
-
-
-
-
-
-
-
-
 //imported settings
 let prefix;
+
+//db
+const firebase = require("firebase/app");
+const FieldValue = require("firebase-admin").firestore.FieldValue;
+const admin = require("firebase-admin");
+const sA = require("./serviceAccount.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(sA)
+});
+let db = admin.firestore();
+
+
+
+
+
+
+
 
 
  //turning bot on/logging it in
